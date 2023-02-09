17928b9f16bf1391b6754b878b15a8cebc3b8f95
Aditya Singh <adityaanurag1969@gmail.com>
2023-02-10 00:18:00 +0530

    Updated Api Files

--- script.js
+++ script.js
@@ -2,32 +2,30 @@ const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '08d9e1acfdmsh99e5ac9de318fd3p1d1455jsn8e5bb0507781',
-		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
 	}
 };
 
 const getweather= (city)=>{
-
-	city_name.innerHTML = city
-fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+    fetch('https://weatherapi-com.p.rapidapi.com/current.json?q=' + city, options)
 	.then(response => response.json())
 	.then((response) => {
 	
 	console.log(response)
-     
-	 
-	 temp.innerHTML = response.temp
-	 temp_block.innerHTML = response.temp
-	 min_temp.innerHTML = response.min_temp
-     max_temp.innerHTML = response.max_temp
-     cloud_pct.innerHTML = response.cloud_pct
-     feels_like.innerHTML = response.feels_like
-     humidity.innerHTML = response.humidity
-	 humidity_block.innerHTML = response.humidity
-     wind_speed.innerHTML = response.wind_speed
-	 wind_speed_block.innerHTML = response.wind_speed
-     wind_degrees.innerHTML = response.wind_degrees
-     sunrise.innerHTML = response.sunrise
+	 city_name.innerHTML = response.location.name
+	 region.innerHTML = response.location.region
+	 temp.innerHTML = response.current.temp_c
+	 temp_block.innerHTML = response.current.temp_c
+	 feel.innerHTML = response.current.feelslike_c
+     uv.innerHTML = response.current.uv
+	 wind_speed.innerHTML = response.current.wind_kph
+	 wind_speed_block.innerHTML = response.current.wind_kph
+     wind_degrees.innerHTML = response.current.wind_degree
+     wind_dir.innerHTML = response.current.wind_dir
+     humidity.innerHTML = response.current.humidity
+	 humidity_block.innerHTML = response.current.humidity
+	 visibility.innerHTML = response.current.vis_km
+	 precip_mm.innerHTML = response.current.precip_mm
      //sunset.innerHTML = response.sunset
 	})
 
